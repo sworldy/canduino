ad21d8ad1433e63a32803d6c452a1c6d4b4862d1
kaipyroami <kaipyroami@gmail.com>
2010-10-27 20:24:46 +0000

    baud.h baud.cpp updates

--- Library/BAUD/baud.h
+++ Library/BAUD/baud.h
@@ -5,39 +5,39 @@ use preprocessor command "_XXkbps"
 "XX" is the baud rate.
 
 10 kbps
-CNF1/BRGCON1  	b'00110001'  	0x31
-CNF2/BRGCON2 	b'10111000' 	0xB8
-CNF3/BRGCON3 	b'00000101' 	0x05
+CNF1/BRGCON1    b'00110001'     0x31
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
 
 20 kbps
-CNF1/BRGCON1  	b'00011000'  	0x18
-CNF2/BRGCON2 	b'10111000' 	0xB8
-CNF3/BRGCON3 	b'00000101' 	0x05
+CNF1/BRGCON1    b'00011000'     0x18
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
 
 50 kbps
-CNF1/BRGCON1  	b'00001001'  	0x09
-CNF2/BRGCON2 	b'10111000' 	0xB8
-CNF3/BRGCON3 	b'00000101' 	0x05
+CNF1/BRGCON1    b'00001001'     0x09
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
 
 100 kbps
-CNF1/BRGCON1  	b'00000100'  	0x04
-CNF2/BRGCON2 	b'10111000' 	0xB8
-CNF3/BRGCON3 	b'00000101' 	0x05
+CNF1/BRGCON1    b'00000100'     0x04
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
 
 125 kbps
-CNF1/BRGCON1  	b'00000011'  	0x03
-CNF2/BRGCON2 	b'10111000' 	0xB8
-CNF3/BRGCON3 	b'00000101' 	0x05
+CNF1/BRGCON1    b'00000011'     0x03
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
 
 250 kbps
-CNF1/BRGCON1  	b'00000001'  	0x01
-CNF2/BRGCON2 	b'10111000' 	0xB8
-CNF3/BRGCON3 	b'00000101' 	0x05
+CNF1/BRGCON1    b'00000001'     0x01
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
 
 500 kbps
-CNF1/BRGCON1  	b'00000000'  	0x00
-CNF2/BRGCON2 	b'10111000' 	0xB8
-CNF3/BRGCON3 	b'00000101' 	0x05
+CNF1/BRGCON1    b'00000000'     0x00
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
 
 800 kbps
 Not yet supported
@@ -47,33 +47,8 @@ Not yet supported
 
 */
 
-#ifdef _10kbps
-#define baud0
-#define baud1
-#define baud2
-#endif
-
-#ifdef _20kbps
-
-#endif
-
-#ifdef _50kbps
-
-#endif
-
-#ifdef _100kbps
-
-#endif
-
-#ifdef _125kbps
-
-#endif
-
-#ifdef _250kbps
-
-#endif
-
-#ifdef _500kbps
-
-#endif
-
+public class baud()
+{
+public:
+static void regConfig();
+};
