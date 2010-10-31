6a1a3fea50bfa83b83730381f70bc96ef18ed0fb
kaipyroami@gmail.com <kaipyroami@gmail.com@e4a586a4-2fee-11df-a3dc-1b164710d598>
2010-10-31 01:01:03 +0000

    Added Baud class to CANClass class

--- trunk/Library/CAN/CAN.cpp
+++ trunk/Library/CAN/CAN.cpp
@@ -1,14 +1,66 @@
+
 #include <CAN.h>
 #include <SPI.h>
 CANClass can;//create can object
 
-void CANClass::bps(byte config1, byte config2, byte config3)//sets bitrate for CAN node
+CANClass::CANClass()//constructor for initializing can module.
 {
+
+}
+
+void CANClass::baudConfig(int bitRate)//sets bitrate for CAN node
+{
+  byte config0, config1, config2;
+
+switch (bitRate)
+{
+case:10
+config0 = 0x31
+config1 = 0xB8
+config2 = 0x05
+break;
+
+case:20
+config0 = 0x18
+config1 = 0xB8
+config2 = 0x05
+break;
+
+case:50
+config0 = 0x09
+config1 = 0xB8
+config2 = 0x05
+break;
+
+case:100
+config0 = 0x04
+config1 = 0xB8
+config2 = 0x05
+break;
+
+case:125
+config0 = 0x03
+config1 = 0xB8
+config2 = 0x05
+break;
+
+case:250
+config0 = 0x01
+config1 = 0xB8
+config2 = 0x05
+break;
+
+case:500
+config0 = 0x00
+config1 = 0xB8
+config2 = 0x05
+break;
+}
   digitalWrite(ss, LOW);
   delay(10);
   SPI.transfer(WRITE);
   SPI.transfer(CNF0);
-  SPI.transfer(config1);
+  SPI.transfer(config0);
   delay(10);
   digitalWrite(ss, HIGH);
   delay(10);
@@ -17,7 +69,7 @@ void CANClass::bps(byte config1, byte config2, byte config3)//sets bitrate for C
   delay(10);
   SPI.transfer(WRITE);
   SPI.transfer(CNF1);
-  SPI.transfer(config2);
+  SPI.transfer(config1);
   delay(10);
   digitalWrite(ss, HIGH);
   delay(10);
@@ -26,7 +78,7 @@ void CANClass::bps(byte config1, byte config2, byte config3)//sets bitrate for C
   delay(10);
   SPI.transfer(WRITE);
   SPI.transfer(CNF2);
-  SPI.transfer(config3);
+  SPI.transfer(config2);
   delay(10);
   digitalWrite(ss, HIGH);
   delay(10);
--- trunk/Library/CAN/CAN.h
+++ trunk/Library/CAN/CAN.h
@@ -1,3 +1,52 @@
+/*
+Created by: Kyle Crockett
+For canduino with 16MHz oscillator.
+CNFx register values.
+use preprocessor command "_XXkbps" 
+"XX" is the baud rate.
+
+10 kbps
+CNF1/BRGCON1    b'00110001'     0x31
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
+
+20 kbps
+CNF1/BRGCON1    b'00011000'     0x18
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
+
+50 kbps
+CNF1/BRGCON1    b'00001001'     0x09
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
+
+100 kbps
+CNF1/BRGCON1    b'00000100'     0x04
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
+
+125 kbps
+CNF1/BRGCON1    b'00000011'     0x03
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
+
+250 kbps
+CNF1/BRGCON1    b'00000001'     0x01
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
+
+500 kbps
+CNF1/BRGCON1    b'00000000'     0x00
+CNF2/BRGCON2    b'10111000'     0xB8
+CNF3/BRGCON3    b'00000101'     0x05
+
+800 kbps
+Not yet supported
+
+1000 kbps
+Not yet supported
+
+*/
 #ifndef
 #define can_h
 class CANClass
@@ -5,7 +54,7 @@ class CANClass
 private:
 
 public:
-static void bps(byte bpsRate);
+static void baudConfig(int bitRate);
 
 static void send_0();
 static void send_1();
