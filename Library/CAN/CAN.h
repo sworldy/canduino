0f33986a0bc6ac22c7796e007c4780fa40435681
kaipyroami <kaipyroami@gmail.com>
2010-10-31 01:06:52 +0000

    Added register definitions to code. Completed constructor.

--- Library/CAN/CAN.h
+++ Library/CAN/CAN.h
@@ -49,6 +49,47 @@ Not yet supported
 */
 #ifndef
 #define can_h
+
+#define SCK 13 //spi
+#define MISO 12
+#define MOSI 11
+#define SS 10
+#define RESET 2//reset pin
+
+#define RESET_REG 0xc0  
+#define READ 0x03 
+#define WRITE 0x02 //read and write comands for SPI
+
+#define READ_RX_BUF_0_ID 0x90
+#define READ_RX_BUF_0_DATA 0x92
+#define READ_RX_BUF_1_ID 0x94
+#define READ_RX_BUF_1_DATA 0x96 //SPI commands for reading CAN RX buffers
+
+#define LOAD_TX_BUF_0_ID 0x40
+#define LOAD_TX_BUF_0_DATA 0x41
+#define LOAD_TX_BUF_1_ID 0x42
+#define LOAD_TX_BUF_1_DATA 0x43
+#define LOAD_TX_BUF_2_ID 0x44
+#define LOAD_TX_BUF_2_DATA 0x45 //SPI commands for loading CAN TX buffers
+
+#define SEND_TX_BUF_0 0x81
+#define SEND_TX_BUF_1 0x82
+#define SEND_TX_BUF_2 0x83 //SPI commands for transmitting CAN TX buffers
+
+#define READ_STATUS 0xA0
+#define RX_STATUS 0xB0
+#define BIT_MODIFY 0x05 //Other commands
+
+   
+//Registers
+#define CNF1 0x2A
+#define CNF2 0x29
+#define CNF3 0x28
+#define TXB0CTRL 0x30 
+#define TXB1CTRL 0x40
+#define TXB2CTRL 0x50 //TRANSMIT BUFFER CONTROL REGISTER
+
+
 class CANClass
 {
 private:
